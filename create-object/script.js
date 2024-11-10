2465e92b29816d0d35f8c8dd16fc76dbeac99ea0
Victoria Shevchenko <shevviktoryia@gmail.com>
2024-11-10 19:43:21 +0300

    feat: add second task

---
+++ create-object/script.js
@@ -0,0 +1,20 @@
+/** ЗАДАЧА 12 - Создание объекта
+ *
+ * 1. Создайте объект с тремя свойствами:
+ *  - name
+ *  - surname
+ *  - favoriteNumber
+ *
+ * 2. Выведите в консоль строку
+ * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
+ */
+
+const obj = {
+  name: "Sam",
+  surname: "Tory",
+  favoriteNumber: "5",
+};
+
+console.log(
+  `My name is ${obj.name} ${obj.surname} and my favorite number is ${obj.favoriteNumber}`
+);
